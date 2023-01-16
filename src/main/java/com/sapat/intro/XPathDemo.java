43a3e2e369c3670853af3a2394ddedf8df892cf0
belekbek <belek.baratov@gmail.com>
2023-01-16 17:10:24 +0600

    ds

--- src/main/java/com/sapat/intro/XPathDemo.java
+++ src/main/java/com/sapat/intro/XPathDemo.java
@@ -1,29 +1,15 @@
 package com.sapat.intro;
 
 import com.github.javafaker.Faker;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 
-public class XPathDemo {
-    WebDriver driver;
-    @BeforeClass
-    public void setup(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        driver.get("https://glovoapp.com/");
-    }
+public class XPathDemo extends BaseTest {
     @Test
     public void signUpToGlovo(){
+        driver.get("https://glovoapp.com/");
         // Creating fake data
         Faker fakeData = new Faker();
         String name = fakeData.name().fullName();
@@ -47,11 +33,4 @@ public class XPathDemo {
         System.out.println("\n"+name + "\n" + mail + "\n" + password);
     }
 
-
-
-    @AfterClass
-    public void close(){
-        driver.close();
-        driver.quit();
-    }
 }
