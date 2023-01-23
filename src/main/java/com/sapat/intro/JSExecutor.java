2b09143760a8ccd07aec0a977fe9f7aec12b78ae
belekbek <belek.baratov@gmail.com>
2023-01-23 15:31:31 +0600

    Merge remote-tracking branch 'origin/master' into ofice
    # Conflicts:
    #	target/classes/com/sapat/intro/IMoving.class

--- src/main/java/com/sapat/intro/JSExecutor.java
+++ src/main/java/com/sapat/intro/JSExecutor.java
@@ -1,11 +1,18 @@
 package com.sapat.intro;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-public class JSExecutor {
+public class JSExecutor extends BaseTest {
 
     @Test
-    public void test1(){
+    public void test1() {
+        driver.navigate().to("https://www.etsy.com/");
+        Helper.jsScrollDownThePage(driver);
+        WebElement element = driver.findElement(By.xpath("//a[@href='https://investors.etsy.com']"));
+        //Helper.jsClick(driver, element);
+        Helper.scrollToFindElement(driver, element);
 
     }
 }
