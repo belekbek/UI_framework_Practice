cbcca985b4332aed1f68c4d7ee3db8c6f6028622
belekbek <belek.baratov@gmail.com>
2023-01-25 20:51:24 +0600

    Merge branch 'master' of https://github.com/belekbek/UI_framework_Practice

--- src/main/java/com/sapat/intro/SeleniumExceptions.java
+++ src/main/java/com/sapat/intro/SeleniumExceptions.java
@@ -1,6 +1,8 @@
 package com.sapat.intro;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class SeleniumExceptions extends BaseTest{
@@ -12,9 +14,29 @@ public class SeleniumExceptions extends BaseTest{
 
     }
     @Test
-    public void test(){
-        browserHelper.openURL("https://demoqa.com/text-box");
-        driver.findElement(By.id("submit")).sendKeys("sdsd");
+    public void elementClickInterceptedException(){
+        browserHelper.openURL("https://demoqa.com/radio-button");
+        driver.findElement(By.id("noRadio")).click();
+    }
+    @Test
+    public void elementNotInteractableException(){
+        browserHelper.openURL("https://demoqa.com/radio-button");
+        driver.findElement(By.id("noRadio")).sendKeys("saas");
+    }
+    @Test
+    public void elementClickInterceptedExceptionTest(){
+        browserHelper.openURL("https://www.imoving.com/");
+        WebElement selectMenu = driver.findElement(By.id("houseTypeSelectList"));
+        Select select = new Select(selectMenu);
+        select.selectByVisibleText("My House");
+        Select select1 = new Select(driver.findElement(By.id("hp-nav-select-house")));
+        select1.selectByVisibleText("Just a few Items");
+        driver.findElement(By.xpath("//button[text()='Compare Quotes']")).click();
+        Helper.pause(3000);
+        driver.findElement(By.xpath("//a[@class='btn btn-blue col-xs-6']")).click();
+        Helper.pause(3000);
+        driver.findElement(By.xpath(".//a[@id='closeTutorial']")).click();
+        Helper.pause(3000);
     }
 
 }
