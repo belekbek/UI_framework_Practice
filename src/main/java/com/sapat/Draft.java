793c2448bd3245cb1ee7fbb257973da79af93efe
belekbek <belek.baratov@gmail.com>
2023-01-30 16:42:37 +0600

    test commit belek

--- src/main/java/com/sapat/Draft.java
+++ src/main/java/com/sapat/Draft.java
@@ -2,6 +2,6 @@ package com.sapat;
 
 public class Draft {
     public static void main(String[] args) {
-        System.out.println("Bele11111k");
+        System.out.println("Bele11111223332k");
     }
 }
