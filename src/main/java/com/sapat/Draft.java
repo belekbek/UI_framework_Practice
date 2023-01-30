9f06f4d9668c8ac670dadae995bf9a8a10f7a808
belekbek <belek.baratov@gmail.com>
2023-01-30 19:15:39 +0600

    git token

--- src/main/java/com/sapat/Draft.java
+++ src/main/java/com/sapat/Draft.java
@@ -4,4 +4,9 @@ public class Draft {
     public static void main(String[] args) {
         System.out.println("Bele11111223332k");
     }
+
+
+
+    //   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
+
 }
