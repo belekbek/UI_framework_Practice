af874ee8556563a6ca04cc1652f8d4541ce1c26a
belekbek <belek.baratov@gmail.com>
2023-02-01 13:09:15 +0600

    commit

--- src/main/java/com/talentLMS/Main.java
+++ src/main/java/com/talentLMS/Main.java
@@ -9,5 +9,6 @@ public class Main {
         Driver.getDriver().get("https://www.amazon.com/");
         Thread.sleep(3000);
         Driver.closeDriver();
+        System.out.println("Belek");
     }
 }
